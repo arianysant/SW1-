cdd6845dc701e178c51137989152d95bff7668fb
Ariany Santos <100931444+arianysant@users.noreply.github.com>
2023-04-04 09:28:55 -0300

    28/03

---
+++ projeto_pessoa/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace projeto_pessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+       Pessoa p1 = new Pessoa();
+       //p1.InsereNome("ariany");
+       //p1.InsereSobreNome("santos");
+       //p1.InsereIdade(16);
+
+       p1.MostraDados();  
+    }
+}
