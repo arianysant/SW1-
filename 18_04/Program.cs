979518d6508b777e2991a56f65268f8383cd6e3f
Ariany Santos <100931444+arianysant@users.noreply.github.com>
2023-05-02 09:55:30 -0300

    Update Program.cs

--- 18_04/Program.cs
+++ 18_04/Program.cs
@@ -3,19 +3,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Projeto Banco ariany andrade");
+        Console.WriteLine("Projeto Banco Ariany Andrade");
         Banco b = new Banco();
 
         b.Depositar(1000);
         b.Sacar(1600);
-      
         b.AjustarLimite(500);
 
         double valortotal = b.MostrarSaldo();
 
         Console.WriteLine("O seu saldo  é de " + valortotal);
         
-
         b.imprimi(b.emprestimo(12,1000));
     }
 }
