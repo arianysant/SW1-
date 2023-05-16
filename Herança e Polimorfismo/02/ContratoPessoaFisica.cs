ed756e02348df04204294491bccd28dc6157f190
Ariany Santos <100931444+arianysant@users.noreply.github.com>
2023-05-16 11:10:05 -0300

    exercicios

---
+++ Herança e Polimorfismo/02/ContratoPessoaFisica.cs
@@ -0,0 +1,13 @@
+namespace _02;
+
+class ContratoPessoaFisica:Contrato
+{
+  public string? Cpf {get;set;}
+   public string? Idade {get;set;}
+   
+
+   public virtual string MostraDados(){
+
+    return base.MostraDados() + " - " +  this.Cpf + "-" + this.Idade;
+}
+}
---
+++ Herança e Polimorfismo/02/ContratoPessoaJuri.cs
@@ -0,0 +1,15 @@
+namespace _02;
+
+class ContratoPessoaJuri:Contrato
+{
+  public string? Cnpj {get;set;}
+   public string? IE {get;set;}
+
+   public string? NomeEmpresa {get;set;}
+   
+
+   public override string MostraDados(){
+
+    return base.MostraDados() + " - " +  this.Cnpj + "-" + this.IE + "-" + this.NomeEmpresa;
+}
+}
