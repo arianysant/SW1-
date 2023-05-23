7caf33f4b193fcbad7d26fe8c3fe1d48767927a6
Ariany Andrade <100931444+arianysant@users.noreply.github.com>
2023-05-23 12:32:36 -0300

    Update ContratoPessoaFisica.cs

--- exercicio_HeP/03/ContratoPessoaFisica.cs
+++ exercicio_HeP/03/ContratoPessoaFisica.cs
@@ -2,10 +2,10 @@ namespace _03;
 class ContratoPessoaFisica:Contrato
 {
   public string? Cpf {get;set;}
-   public string? Idade {get;set;}
+   public int Idade {get;set;}
    
 
-   public virtual string MostraDados(){
+   public override string MostraDados(){
 
     return base.MostraDados() + " - " +  this.Cpf + "-" + this.Idade;
  }
